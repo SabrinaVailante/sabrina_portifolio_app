435758205376c6a9b4ff009cfdc48d599b8c1230
Sabrina Vailante <sabrina.vailante@defendoria.mg.def.br>
2024-05-06 11:02:15 -0300

    feat(hobbies): Começa e cria a pagima de hobbies.

--- lib/sabrina_portifolio_app/feature/hobbies/view/hobbies_view.dart
+++ lib/sabrina_portifolio_app/feature/hobbies/view/hobbies_view.dart
@@ -10,6 +10,35 @@ class HobbiesView extends StatefulWidget {
 class _HobbiesViewState extends State<HobbiesView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(0, 0, 0, 1.0),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(36, 167, 174, 1.0),
+        title: Text(
+          'Hobbies',
+          style: TextStyle(
+              color: Color.fromRGBO(0, 0, 0, 1.0), fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Row(children: [
+        _buildCircleHobby(Icon(Icons.music_note, color: Colors.white)),
+        _buildCircleHobby(Icon(Icons.pets, color: Colors.white)),
+        _buildCircleHobby(Icon(Icons.games, color: Colors.white)),
+        _buildCircleHobby(Icon(Icons.music_note, color: Colors.white)),
+        _buildCircleHobby(Icon(Icons.pets, color: Colors.white)),
+
+      ]),
+    );
+  }
+
+  Widget _buildCircleHobby(Icon icon) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: CircleAvatar(
+        child: icon,
+        radius: 30,
+        backgroundColor: Color.fromRGBO(36, 167, 174, 1.0),
+      ),
+    );
   }
 }
